a27c7664cdb8c542a37d9632b229f3312d66d93e
Julia Frazão <juliafrazao@MacBook-Air-de-Julia.local>
2024-08-11 19:07:28 -0300

    terceiro passo

--- arquivo.py
+++ arquivo.py
@@ -3,3 +3,14 @@ def cumprimento():
     return f"Olá, {texto}"
 
 print(cumprimento())
+
+import random
+
+def media_numeros_aleatorios():
+    numeros = [random.randint(1, 100) for _ in range(3)]
+    media = sum(numeros) / len(numeros)
+    return numeros, media
+
+numeros_sorteados, media = media_numeros_aleatorios()
+print(f"Números sorteados: {numeros_sorteados}")
+print(f"Média: {media}")
